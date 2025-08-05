11b744fc072083613e211f1c06b5e4652f533415
THEGENTLEMAN31 <bongatchingajose@gmail.com>
2025-08-05 17:11:29 +0100

    fix input
    Right now , the input work

--- index.js
+++ index.js
@@ -3,18 +3,22 @@ const inputBtn =document.getElementById("input-btn")
 const inputEl=document.getElementById("input-el")
 const ulEl =document.getElementById("ul-el")
 
+
+
 inputBtn.addEventListener("click", function()
 {
     myLeads.push(inputEl.value)
+    render()
      
 })
 
-for (let i = 0; i < myLeads.length; i++) {
-    //ulEl.innerHTML += "<li>" + myLeads[i] + "</li>"
-
-    let li =document.createElement("li")
-    li.innerHTML = myLeads[i]
-    ulEl.append(li)
-
+function render()
+{
+        let listItems = ""
+        for (let i = 0; i < myLeads.length; i++) {
+        listItems += "<li>" + myLeads[i] + "</li>"
 }
+ulEl.innerHTML = listItems
+}
+
 
