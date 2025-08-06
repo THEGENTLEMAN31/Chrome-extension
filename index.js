09d5bb7d1e9e9990a26d890f598e64f41423c26d
THEGENTLEMAN31 <bongatchingajose@gmail.com>
2025-08-06 21:41:27 +0100

    tab button
    I have add the tab buttton but  itdoesn't work , i don't know why

--- index.js
+++ index.js
@@ -3,45 +3,51 @@ const inputBtn =document.getElementById("input-btn")
 const inputEl=document.getElementById("input-el")
 const ulEl =document.getElementById("ul-el")
 const deleteBtn = document.getElementById("delete-btn")
+const tabBtn =document.getElementById("tab-btn")
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
 
-/*const leadsFromLocalStorage = JSON.parse(localStoragegetItem("myLeads"))
-if (leadsFromLocalStorage)
-{
-    myLeads=leadsFromLocalStorage
-    renderLeads
-}*/
+if (leadsFromLocalStorage) {
+    myLeads = leadsFromLocalStorage
+    render(myLeads)
+}
 
-inputBtn.addEventListener("click", function()
-{
+inputBtn.addEventListener("click", function(){
     myLeads.push(inputEl.value)
-    renderLeads()
+    render(myLeads) 
     localStorage.setItem("myLeads",JSON.stringify(myLeads))
     inputEl.value =""
-  
 })
-deleteBtn.addEventListener("dblclick", function()
-{
+
+
+deleteBtn.addEventListener("dblclick", function(){
     localStorage.clear()
     myLeads=[]
-    renderLeads()
+    render(myLeads)
 }
 )
 
 
+tabBtn.addEventListener("click", function(){
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+    myLeads.push(tabs[0].url)
+    localStorage.setItem("myLeads", JSON.stringify(myLeads) )
+    render(myLeads)            
+    })
 
-function renderLeads()
-{
-        let listItems = ""
-        for (let i = 0; i < myLeads.length; i++) {
-        
-       
-        
-       listItems += `
-       <li>
-            <a target='_blank' href='${myLeads[i]}'>
-             ${myLeads[i]}
-             </a>
-       </li>`
+    
+})
+
+
+
+function render(leads){
+    let listItems = ""
+    for (let i = 0; i < leads.length; i++) {  
+        listItems += `
+        <li>
+                <a target='_blank' href='${leads[i]}'>
+                ${leads[i]}
+                </a>
+        </li>`
 }
 ulEl.innerHTML = listItems
 }
