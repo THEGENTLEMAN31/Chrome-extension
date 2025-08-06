c4fb8b349b50f39d5e8317c6af34dd46da4bbb41
THEGENTLEMAN31 <bongatchingajose@gmail.com>
2025-08-06 21:48:11 +0100

    last version

--- index.js
+++ index.js
@@ -1,55 +1,47 @@
-let myLeads= []
-const inputBtn =document.getElementById("input-btn")
-const inputEl=document.getElementById("input-el")
-const ulEl =document.getElementById("ul-el")
+let myLeads = []
+const inputEl = document.getElementById("input-el")
+const inputBtn = document.getElementById("input-btn")
+const ulEl = document.getElementById("ul-el")
 const deleteBtn = document.getElementById("delete-btn")
-const tabBtn =document.getElementById("tab-btn")
-const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
+const leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads") )
+const tabBtn = document.getElementById("tab-btn")
 
 if (leadsFromLocalStorage) {
     myLeads = leadsFromLocalStorage
     render(myLeads)
 }
 
-inputBtn.addEventListener("click", function(){
-    myLeads.push(inputEl.value)
-    render(myLeads) 
-    localStorage.setItem("myLeads",JSON.stringify(myLeads))
-    inputEl.value =""
-})
-
-
-deleteBtn.addEventListener("dblclick", function(){
-    localStorage.clear()
-    myLeads=[]
-    render(myLeads)
-}
-)
-
-
-tabBtn.addEventListener("click", function(){
+tabBtn.addEventListener("click", function(){    
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
-    myLeads.push(tabs[0].url)
-    localStorage.setItem("myLeads", JSON.stringify(myLeads) )
-    render(myLeads)            
+        myLeads.push(tabs[0].url)
+        localStorage.setItem("myLeads", JSON.stringify(myLeads) )
+        render(myLeads)
     })
-
-    
 })
 
-
-
-function render(leads){
+function render(leads) {
     let listItems = ""
-    for (let i = 0; i < leads.length; i++) {  
+    for (let i = 0; i < leads.length; i++) {
         listItems += `
-        <li>
+            <li>
                 <a target='_blank' href='${leads[i]}'>
-                ${leads[i]}
+                    ${leads[i]}
                 </a>
-        </li>`
-}
-ulEl.innerHTML = listItems
+            </li>
+        `
+    }
+    ulEl.innerHTML = listItems
 }
 
+deleteBtn.addEventListener("dblclick", function() {
+    localStorage.clear()
+    myLeads = []
+    render(myLeads)
+})
 
+inputBtn.addEventListener("click", function() {
+    myLeads.push(inputEl.value)
+    inputEl.value = ""
+    localStorage.setItem("myLeads", JSON.stringify(myLeads) )
+    render(myLeads)
+})
